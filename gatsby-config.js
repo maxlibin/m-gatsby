8a1889c8ec5aff73ec7692ee520bcf42d1d938f5
maxlibin <me@maxlibin.com>
2019-03-24 12:22:30 +0800

    use https

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,16 +4,11 @@ module.exports = {
     subtitle: `Data fetched from a site hosted on wordpress.com`,
   },
   plugins: [
-    // https://public-api.wordpress.com/wp/v2/sites/gatsbyjsexamplewordpress.wordpress.com/pages/
-    /*
-     * Gatsby's data processing layer begins with “source”
-     * plugins. Here the site sources its data from Wordpress.
-     */
     {
       resolve: `gatsby-source-wordpress`,
       options: {
         baseUrl: `blog.maxlibin.com`,
-        protocol: `http`,
+        protocol: `https`,
         hostingWPCOM: false,
         useACF: false,
         verboseOutput: true,
@@ -27,21 +22,18 @@ module.exports = {
           "**/taxonomies",
           "**/users",
         ],
-        excludedRoutes: [
-          "/wp/v2/search",
-          "/wp/v2/block-renderer",
-        ],
+        excludedRoutes: ["/wp/v2/search", "/wp/v2/block-renderer"],
       },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-55191105-2"
+        trackingId: "UA-55191105-2",
       },
     },
     `gatsby-plugin-favicon`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-glamor`
+    `gatsby-plugin-glamor`,
   ],
 }
