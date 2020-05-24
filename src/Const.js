8883723991ed318af06539f53c8f8a77c6579636
max <me@maxlibin.com>
2020-05-25 02:51:59 +0800

    Update title

--- src/Const.js
+++ src/Const.js
@@ -1,4 +1,3 @@
-export const title =
-  "Max li bin - Frontend, Javascript, React, ReasonMl, ReasonReact developer based in Singapore"
+export const title = "Max li bin - Frontend developer"
 export const description =
   "I am based in Singapore, have Javascript, React, ReasonMl, ReasonReact projects you'd like to discuss?"
